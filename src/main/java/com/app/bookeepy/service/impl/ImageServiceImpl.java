4f088f2528f2fba47e414bd99b84c7a869887830
Pete J. Chiboleca <pete9450@gmail.com>
2021-03-24 10:21:41 +0200

    Some changes

--- src/main/java/com/app/bookeepy/service/impl/ImageServiceImpl.java
+++ src/main/java/com/app/bookeepy/service/impl/ImageServiceImpl.java
@@ -1,15 +1,5 @@
 package com.app.bookeepy.service.impl;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import javax.transaction.Transactional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.app.bookeepy.entity.Book;
 import com.app.bookeepy.entity.Image;
 import com.app.bookeepy.exceptions.ImageException;
@@ -17,98 +7,120 @@ import com.app.bookeepy.exceptions.ImageNotFoundException;
 import com.app.bookeepy.repository.ImageRepository;
 import com.app.bookeepy.service.BookService;
 import com.app.bookeepy.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class ImageServiceImpl implements ImageService {
 
-	@Autowired
-	BookService bookService;
+    @Autowired
+    BookService bookService;
 
-	@Autowired
-	ImageRepository imageRepository;
+    @Autowired
+    ImageRepository imageRepository;
 
-	@PersistenceContext
-	EntityManager em;
+    @PersistenceContext
+    EntityManager em;
 
-	@Override
-	@Transactional
-	public List<Image> addImages(Long bookId, List<Image> images) {
-		Book book = bookService.findBookById(bookId);
-		
-		// Checks wether the new images has a cover
-		Long count = images.stream().filter(image -> image.getIsCover().equals(Boolean.TRUE)).count();
-		
-		if (count >= 2L) throw new ImageException("Error: A book cannot have two covers!");
-		
-		// If the new images has a cover, then unset the old cover and deletes the Default Location if is there
-		if (count.equals(1L)) {
-			final String removeSQL = "UPDATE images SET is_cover = FALSE WHERE book_id = :id AND is_cover = TRUE";
-			Query query = em.createNativeQuery(removeSQL);
-			query.setParameter("id", bookId);
-			query.executeUpdate();
-			
-			final String removeDefaultCoverSQL = "DELETE FROM images WHERE image_location = 'Default Location' AND book_id = :id";
-			query = em.createNativeQuery(removeDefaultCoverSQL);
-			query.setParameter("id", bookId);
-			query.executeUpdate();
-		}
-		
-		book.addImages(images);
-		/*images.forEach(image -> image.setBook(book));
-		return imageRepository.saveAll(images);*/
-		return book.getImages();
-	}
+    @Override
+    @Transactional
+    public List<Image> addImages(Long bookId, List<Image> images) {
+        Book book = bookService.findBookById(bookId);
 
-	@Override
-	@Transactional
-	public Image addImage(Long bookId, Image image) {
-		Book book = bookService.findBookById(bookId);
-		book.addImage(image);
-		
-		int index = book.getImages().indexOf(image);
+        // Checks wether the new images has a cover
+        Long count = images.stream().filter(image -> image.getIsCover().equals(Boolean.TRUE)).count();
 
-		return book.getImages().get(index);
-	}
+        if (count >= 2L) throw new ImageException("Error: A book cannot have two covers!");
 
-	@Override
-	public void removeImage(Long id) {
-		Image image = findImageById(id);
-		imageRepository.delete(image);
-	}
+        // If the new images has a cover, then unset the old cover and deletes the Default Location if is there
+        if (count.equals(1L)) {
+            final String removeSQL = "UPDATE images SET is_cover = FALSE WHERE book_id = :id AND is_cover = TRUE";
+            Query query = em.createNativeQuery(removeSQL);
+            query.setParameter("id", bookId);
+            query.executeUpdate();
 
-	@Override
-	public Image findImageById(Long id) {
-		return imageRepository.findById(id)
-				.orElseThrow(() -> new ImageNotFoundException("Error: Could not find an image with this id: " + id));
-	}
+            final String removeDefaultCoverSQL = "DELETE FROM images WHERE image_location = :location AND book_id = :id";
+            Query deleteDefault = em.createNativeQuery(removeDefaultCoverSQL);
+            deleteDefault.setParameter("id", bookId);
+            deleteDefault.setParameter("location", "Default Location");
+            deleteDefault.executeUpdate();
+        }
 
-	@Override
-	@Transactional
-	public Image setCoverPicture(Long bookId, Long imageId) {
+        images.forEach(image -> image.setBook(book));
+        return imageRepository.saveAll(images);
+    }
 
-		Book book = bookService.findBookById(bookId);
+    @Override
+    @Transactional
+    public Image addImage(Long bookId, Image image) {
+        Book book = bookService.findBookById(bookId);
 
-		final String removeSQL = "UPDATE images SET is_cover = FALSE WHERE book_id = :id AND is_cover = TRUE";
-		Query query = em.createNativeQuery(removeSQL);
-		query.setParameter("id", bookId);
-		query.executeUpdate();
-		
-		final String removeDefaultCoverSQL = "DELETE FROM images WHERE image_location = 'Default Location' AND book_id = :id";
-		query = em.createNativeQuery(removeDefaultCoverSQL);
-		query.setParameter("id", bookId);
-		query.executeUpdate();
-		
+        // If The new image is cover, then remove old cover and default cover if exists
+        if (image.getIsCover()) {
+            final String removeSQL = "UPDATE images SET is_cover = FALSE WHERE book_id = :id AND is_cover = TRUE";
+            Query query = em.createNativeQuery(removeSQL);
+            query.setParameter("id", bookId);
+            query.executeUpdate();
 
-		Image image = findImageById(imageId);
+            final String removeDefaultCoverSQL = "DELETE FROM images WHERE image_location = :location AND book_id = :id";
+            Query deleteDefault = em.createNativeQuery(removeDefaultCoverSQL);
+            deleteDefault.setParameter("id", bookId);
+            deleteDefault.setParameter("location", "Default Location");
+            deleteDefault.executeUpdate();
+        }
 
-		image.setIsCover(Boolean.TRUE);
+        book.addImage(image);
 
-		return imageRepository.save(image);
-	}
+        int index = book.getImages().indexOf(image);
 
-	@Override
-	public void deleteSome(Integer[] ids) {
-		imageRepository.deleteSome(ids);
-	}
+        return book.getImages().get(index);
+    }
+
+    @Override
+    public void removeImage(Long id) {
+        Image image = findImageById(id);
+        imageRepository.delete(image);
+    }
+
+    @Override
+    public Image findImageById(Long id) {
+        return imageRepository.findById(id)
+                .orElseThrow(() -> new ImageNotFoundException("Error: Could not find an image with this id: " + id));
+    }
+
+    @Override
+    @Transactional
+    public Image setCoverPicture(Long bookId, Long imageId) {
+
+        Book book = bookService.findBookById(bookId);
+
+        final String removeSQL = "UPDATE images SET is_cover = FALSE WHERE book_id = :id AND is_cover = TRUE";
+        Query query = em.createNativeQuery(removeSQL);
+        query.setParameter("id", bookId);
+        query.executeUpdate();
+
+        final String removeDefaultCoverSQL = "DELETE FROM images WHERE image_location = 'Default Location' AND book_id = :id";
+        query = em.createNativeQuery(removeDefaultCoverSQL);
+        query.setParameter("id", bookId);
+        query.executeUpdate();
+
+
+        Image image = findImageById(imageId);
+
+        image.setIsCover(Boolean.TRUE);
+
+        return imageRepository.save(image);
+    }
+
+    @Override
+    public void deleteSome(Integer[] ids) {
+        imageRepository.deleteSome(ids);
+    }
 
 }
